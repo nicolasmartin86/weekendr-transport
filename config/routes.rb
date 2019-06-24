e6bf8f47797ca394e9a55476c947efb5f08448f3
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-06-24 10:25:32 +0200

    create controller for index destinations and seed start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'destinations#index'
+  get '/bon-plans', to: 'destinations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Destination.destroy_all
+
+
+puts 'Creating awesome destinations...'
+
+Destination.create!( {
+    destination_name: "Madrid Espagne",
+    transport_price: 10.0,
+    photo_link: "https://images.kiwi.com/photos/600x600/madrid_es.jpg"
+})
+
+
+Destination.create!( {
+    destination_name: "Barcelona Espagne",
+    transport_price: 8.50,
+    photo_link: "https://images.kiwi.com/photos/600x600/barcelona_es.jpg"
+})
+
+
+number = Destination.all.size
+
+puts "--------------------------------"
+puts "--------------------------------"
+puts "#{number} destinations created!"
+puts "--------------------------------"
+puts "--------------------------------"
+puts "Creation Finished!"
+puts "--------------------------------"
