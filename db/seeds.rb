0bfe442964caab707f52e34ad86a47f9d505e0dc
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-06-24 16:56:39 +0200

    fix seed and finish responsive front page plus cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'open-uri'
+require "net/http"
 require 'json'
 require 'nokogiri'
 
@@ -37,8 +38,13 @@ cities.each do |city|
   city[:city] = full_names.first.strip
   clean_country_name = full_names.last.strip
   city[:country] = clean_country_name
-  found_country = country_json.select { |element| (element["Name"].upcase == clean_country_name.upcase) || (element["Name"].upcase.include?(clean_country_name.upcase)) }
-  unless found_country == []
+  found_country = country_json.select { |element| element["Name"].upcase == clean_country_name.upcase }
+  almost_found_country = country_json.select { |element| element["Name"].upcase.include?(clean_country_name.upcase) }
+  if found_country == []
+    unless almost_found_country == []
+      city[:code_country] = almost_found_country[0]["Code"]
+    end
+  else
     city[:code_country] = found_country[0]["Code"]
   end
 end
@@ -56,15 +62,24 @@ puts 'Creating awesome destinations...'
 
 cities_clean.each do |city|
   if city[:city].scan(" ")
-    city_name_photo = city[:city].downcase.sub(" ", "-")
+    city_name_photo = city[:city].downcase.gsub(" ", "-")
   else
     city_name_photo = city[:city].downcase
   end
-  Destination.create!( {
-      destination_name: city[:city_full_name],
-      transport_price: city[:transport_price],
-      photo_link: "https://images.kiwi.com/photos/600x600/#{city_name_photo}_#{city[:code_country].downcase}.jpg"
-  })
+
+  url_photo = "https://images.kiwi.com/photos/600x600/#{city_name_photo}_#{city[:code_country].downcase}.jpg"
+  uri_photo = URI(url_photo)
+
+  if Net::HTTP.get_response(uri_photo).code == "200"
+    Destination.create!( {
+        destination_name: city[:city_full_name],
+        transport_price: city[:transport_price],
+        photo_link: url_photo
+    })
+    puts "#{city[:city_full_name]} created!"
+  else
+    puts "#{city[:city_full_name]} KO, picture not found!"
+  end
 end
 
 number = Destination.all.size
@@ -76,3 +91,6 @@ puts "--------------------------------"
 puts "--------------------------------"
 puts "Creation Finished!"
 puts "--------------------------------"
+
+
+
