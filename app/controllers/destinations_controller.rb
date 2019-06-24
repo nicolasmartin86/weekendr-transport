c8a59bb962f6007ca71bb1348fc6e3bbb3a11ecc
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-06-24 22:00:21 +0200

    reorder destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,6 @@
 class DestinationsController < ApplicationController
   def index
-    @destinations_asked = Destination.where(destination_name: Destination.destinations_asked)
-    @destinations = Destination.where.not(destination_name: Destination.destinations_asked)
+    @destinations_asked = Destination.where(destination_name: Destination.destinations_asked).order(destination_name: :asc)
+    @destinations = Destination.where.not(destination_name: Destination.destinations_asked).order(destination_name: :asc)
   end
 end
